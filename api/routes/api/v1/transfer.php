b32c5d9468b18f61805bece7388a3f196566ddb7
Reginaldo <reginaldo.moura@hotmail.com.br>
2022-08-21 15:56:01 -0400

    fix(transfer): fix route prefix .

--- api/routes/api/v1/transfer.php
+++ api/routes/api/v1/transfer.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\V1\TransferController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('transfer')->middleware(['auth:sanctum', 'can.abilities:can-transfer'])->group(function () {
+Route::prefix('transfers')->middleware(['auth:sanctum', 'can.abilities:can-transfer'])->group(function () {
     Route::post('', [TransferController::class, 'transfer']);
 });
