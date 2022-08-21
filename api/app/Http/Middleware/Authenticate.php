ee554b9eef331cda34821dfa13037e3b0362977f
Reginaldo <reginaldo.moura@hotmail.com.br>
2022-08-21 15:31:21 -0400

    chore: change middleware to response json.

--- api/app/Http/Middleware/Authenticate.php
+++ api/app/Http/Middleware/Authenticate.php
@@ -3,19 +3,35 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return string|null
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (empty($guards)) {
+            $guards = [null];
         }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                $this->auth->shouldUse($guard);
+                return $request;
+            }
+        }
+
+        throw new UnauthorizedHttpException(
+            '',
+            __('exception.auth.no_session'),
+            null,
+            HttpFoundation\Response::HTTP_UNAUTHORIZED
+        );
     }
 }
