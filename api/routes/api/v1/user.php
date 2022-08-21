c4040892547ef84c1630dcfed0f58f949d322998
Reginaldo <reginaldo.moura@hotmail.com.br>
2022-08-21 17:42:54 -0400

    feat(users): added middleware auth.

--- api/routes/api/v1/user.php
+++ api/routes/api/v1/user.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('users')->group(function() {
+Route::prefix('users')->middleware('auth:sanctum')->group(function() {
     Route::post('', [UserController::class, 'create']);
     Route::get('', [UserController::class, 'get']);
 });
