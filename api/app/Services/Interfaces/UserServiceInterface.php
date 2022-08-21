64d2c4d678b03cdfb63ee8807b1ce776c700cc31
Reginaldo <reginaldo.moura@hotmail.com.br>
2022-08-21 17:58:29 -0400

    chore(users): update service interface

--- api/app/Services/Interfaces/UserServiceInterface.php
+++ api/app/Services/Interfaces/UserServiceInterface.php
@@ -2,10 +2,13 @@
 
 namespace App\Services\Interfaces;
 
+use App\Models\User;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface UserServiceInterface
 {
+    public function create(array $attributes): User;
+
     public function findUserByUuid(string $uuid);
 
     public function findUsers(): LengthAwarePaginator;
