832c04c1db763c87d9c688497bc41864f28562de
Reginaldo <reginaldo.moura@hotmail.com.br>
2022-08-21 20:52:45 -0400

    fix(services): fix base uri.

--- api/config/services.php
+++ api/config/services.php
@@ -32,7 +32,7 @@ return [
     ],
 
     'external_authorizer' => [
-        'base_uri' => env('EXTERNAL_AUTHORIZED_URI', 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6/'),
+        'base_uri' => env('EXTERNAL_AUTHORIZED_URI', 'https://run.mocky.io'),
     ],
 
     'external_notification' => [
