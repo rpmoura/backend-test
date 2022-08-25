c1f9d1952627a4462e030cf3e2fbc675a6134cee
Reginaldo <reginaldo.moura@hotmail.com.br>
2022-08-25 08:18:39 -0400

    chore(transfer): remove transfer model

--- api/app/Http/Controllers/V1/TransferController.php
+++ api/app/Http/Controllers/V1/TransferController.php
@@ -8,7 +8,7 @@ use App\Exceptions\UnauthorizedTransferException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TransferRequest;
 use App\Http\Resources\WalletResource;
-use App\Services\Interfaces\{TransferServiceInterface, UserServiceInterface, WalletServiceInterface};
+use App\Services\Interfaces\{UserServiceInterface, WalletServiceInterface};
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,7 +19,6 @@ use Symfony\Component\HttpFoundation\Response;
 class TransferController extends Controller
 {
     public function __construct(
-        protected TransferServiceInterface $transferService,
         protected WalletServiceInterface $walletService,
         protected UserServiceInterface $userService
     ) {}
